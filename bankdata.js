9977fba5c403fc12daa67d2dd7d2d05e2eaf0311
M19604 <141754314+M19604@users.noreply.github.com>
2024-10-29 18:00:47 -0400

    Update bankdata.js

--- bankdata.js
+++ bankdata.js
@@ -9,10 +9,10 @@ const clientNames = {
     germany:"New Germany"
 }
 var clientData = {}
-var serverURL1 = "https://sheetdb.io/api/v1/60kwt987z708g"; // jack.lawson20060@gmail.com
+var serverURL1 = "https://sheetdb.io/api/v1/60kwt987z708g";
 var serverURL2 = "https://sheetdb.io/api/v1/c6fsu6g25zbol"; // usabankingserver1@gmail.com
-var serverURL3 = "https://sheetdb.io/api/v1/dhfrn9eco3t9u"; // usabankingserver2@gmail.com
-var serverURL4 = "https://sheetdb.io/api/v1/ksgoxe1j3a7gz"; // globalbanking994@gmail.com
+var serverURL3 = "https://sheetdb.io/api/v1/ksgoxe1j3a7gz"; // usabankingserver2@gmail.com
+var serverURL4 = "";
 var serverURL5 = "";
 function updateData() {
 fetch(serverURL1).then(response => {
@@ -45,19 +45,9 @@ var y = data[0][x];
 clientData[x] = y;
 }
 }).catch(err => {
-fetch(serverURL4).then(response => {
-console.log("Server 3 connected.");
-return response.json();
-}).then(data => {
-for(let i = 0; i < Object.keys(clientNames).length; i++){
-var x = Object.keys(clientNames)[i];
-var y = data[0][x];
-clientData[x] = y;
-}
-}).catch(err => {
 alert("We ran into an error! Please contact 012578@clioschools.net for support.");
-})
-})
-})
+});
+});
+});
 }
 // WRITTEN BY LUKE SKUTT 10/16/2024
